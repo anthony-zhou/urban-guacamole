e4400de5d0d178d57cb3a874af6d2782af42c6c2
anthony-zhou <anthony.zhou03@gmail.com>
2019-03-26 15:09:45 -0500

    Set up java project

---
+++ MessagingApp/src/Server.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test woohoo");
+	}
+
+}
